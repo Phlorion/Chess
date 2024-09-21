50a45def61552ab14e711370f5c6567e1f7edecd
VaitsisNikolas <vnikolas21@gmail.com>
2024-09-21 17:58:22 +0300

    Nikolas V v1.2 - Castle and Capturing move bug fixed

--- Chess/src/main/java/com/example/scrap/other/TestBoard.java
+++ Chess/src/main/java/com/example/scrap/other/TestBoard.java
@@ -1,8 +1,49 @@
 package com.example.scrap.other;
 
+import com.example.chess.board.Board;
+import com.example.chess.piece.*;
+import com.example.chess.player.Player;
+
+import static com.example.chess.board.Board.NUM_TILES_PER_ROW;
+
 public class TestBoard {
 
     public static void main(String args[]) {
+        Board board = new Board();
+
+//        for (int i = 0; i <8 ; i++) {
+//            for (int j = 0; j < 8; j++) {
+//                board.getBoard()[NUM_TILES_PER_ROW* i + j].setI(i);
+//                board.getBoard()[NUM_TILES_PER_ROW* i + j].setJ(j);
+//                //piece is already null - I think
+//            }
+//        }
+        board.setPieceOnBoard(new Rook(PiecesType.BLACK),0,0);
+        board.setPieceOnBoard(new Rook(PiecesType.BLACK),0,2);
+        board.setPieceOnBoard(new Rook(PiecesType.BLACK),0,7);
+        board.setPieceOnBoard(new Knight(PiecesType.BLACK),0,1);
+        board.setPieceOnBoard(new King(PiecesType.BLACK),0,4);
+        board.setPieceOnBoard(new Queen(PiecesType.BLACK),1,4);
+        board.setPieceOnBoard(new King(PiecesType.WHITE),7,4);
+        board.setPieceOnBoard(new Rook(PiecesType.WHITE),7,0);
+        board.setPieceOnBoard(new Rook(PiecesType.WHITE),7,7);
+
+        Player whitePlayer = board.getWhitePlayer();
+        Player blackPlayer = board.getBlackPlayer();
+
+        board.setBlackPieces(board.findAllActivePieces(board.getBoard(), PiecesType.BLACK));
+        board.setWhitePieces(board.findAllActivePieces(board.getBoard(), PiecesType.WHITE));
+
+        whitePlayer.setPotentialMoves(whitePlayer.calculateAllPotentialMoves(board.getBoard()));
+        blackPlayer.setPotentialMoves(blackPlayer.calculateAllPotentialMoves(board.getBoard()));
+
+        whitePlayer.setLegalMoves(whitePlayer.calculateAllLegalMoves(board.getBoard()));
+        blackPlayer.setLegalMoves(blackPlayer.calculateAllLegalMoves(board.getBoard()));
+
+        System.out.println(board);
+        System.out.println(blackPlayer.getLegalMoves());
+        System.out.println(whitePlayer.getLegalMoves());
+
         // pieces
         /*Knight nBlack = new Knight(1, 1, PiecesType.BLACK);
         King kBlack = new King(0, 1, PiecesType.BLACK);
